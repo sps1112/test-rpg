b190e3d53c8b9b9185f0eac2ab2847a51e16215a
sps1112 <sidps1112@gmail.com>
2020-12-17 22:21:09 +0530

    Initialize standard game loop

--- includes/Standard.h
+++ includes/Standard.h
@@ -13,9 +13,40 @@ void Log(std::string message)
     Log(message.c_str());
 }
 
-void Log(const float num)
+void Log(const float num, int isChar = 0)
 {
-    Log(std::to_string(num));
+    if (isChar)
+    {
+        char c = int(num);
+        Log(std::string(1, c));
+    }
+    else
+    {
+        Log(std::to_string(num));
+    }
+}
+
+void Print(const char *message)
+{
+    std::cout << message;
+}
+
+void Print(std::string message)
+{
+    Print(message.c_str());
+}
+
+void Print(const float num, int isChar = 0)
+{
+    if (isChar)
+    {
+        char c = int(num);
+        Print(std::string(1, c));
+    }
+    else
+    {
+        Print(std::to_string(num));
+    }
 }
 
 #endif
--- src/Application.cpp
+++ src/Application.cpp
@@ -1,15 +1,125 @@
 #include <iostream>
 
 #include "../includes/Standard.h"
+#include "../includes/Printer.h"
+
+void RunMainMenu();
+void RunGame();
+void RunOptions();
+void RunBattle();
 
 int main()
 {
-    Log("Main Menu");
-    Log("1. New Game");
-    Log("2. Options");
-    Log("3. Quit");
-    std::string str = "Please Choose an Option";
-    Log(str);
-    Log(1);
+    rpgPrinter::PrintTitle();
+    RunMainMenu();
+    rpgPrinter::PrintEnd();
+    int choice;
+    std::cin >> choice;
     return 0;
+}
+
+void RunMainMenu()
+{
+    bool hasSavedGame = false;
+    bool hasChosen = false;
+    int choice;
+    rpgPrinter::PrintMenu(hasSavedGame);
+    while (!hasChosen)
+    {
+        std::cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            hasChosen = true;
+            break;
+        case 2:
+            hasChosen = true;
+            break;
+        case 3:
+            hasChosen = true;
+            break;
+        default:
+            hasChosen = false;
+            Print("Select Again: ");
+            break;
+        }
+    }
+    Log("");
+    switch (choice)
+    {
+    case 1:
+        RunGame();
+        break;
+    case 2:
+        RunOptions();
+        break;
+    case 3:
+        break;
+    }
+}
+
+void RunGame()
+{
+    bool isPlaying = true;
+    int playChoice;
+    while (isPlaying)
+    {
+        Log("Game is Running");
+        Log("");
+        Log("Enter 1 to continue or 0 to go back");
+        playChoice = 2;
+        while (playChoice != 0 && playChoice != 1)
+        {
+            std::cin >> playChoice;
+            switch (playChoice)
+            {
+            case 0:
+                isPlaying = false;
+                break;
+            case 1:
+                isPlaying = true;
+                break;
+            default:
+                Print("Select again (1 to continue or 0 to go back): ");
+                break;
+            }
+        }
+    }
+    Log("");
+    RunMainMenu();
+}
+
+void RunOptions()
+{
+    bool isOnOptions = true;
+    int optionChoice;
+    while (isOnOptions)
+    {
+        Log("Options is Running");
+        Log("");
+        Log("Enter 1 to continue or 0 to go back");
+        optionChoice = 2;
+        while (optionChoice != 0 && optionChoice != 1)
+        {
+            std::cin >> optionChoice;
+            switch (optionChoice)
+            {
+            case 0:
+                isOnOptions = false;
+                break;
+            case 1:
+                isOnOptions = true;
+                break;
+            default:
+                Print("Select again (1 to continue or 0 to go back): ");
+                break;
+            }
+        }
+    }
+    RunMainMenu();
+}
+
+void RunBattle()
+{
+    // Battle code
 }

b33c0dea26c811ba944f3cc029cd28fb70276625
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-12-22 19:31:11 +0530

    Add functions to game class

--- test-rpg/includes/Game.h
+++ test-rpg/includes/Game.h
@@ -4,14 +4,63 @@
 // Custom Headers
 #include <Standard.h>
 #include <Printer.h>
+#include <Character.h>
+#include <FileSystem.h>
+#include <Battle.h>
 
-// Game Class
-class Game
+namespace rpgText
 {
-private:
-public:
-    // Default Game Class
-    Game();
-};
+    // All the game modes
+    enum GAME_STATE
+    {
+        STATE_MENU,    // Main Menu
+        STATE_OPTIONS, // Options Menu
+        STATE_QUIT,    // Quit Menu
+        STATE_MAP,     // Map Menu
+        STATE_BATTLE,  // Battle Menu
+        STATE_PAUSE,   // Pause Menu
+    };
+
+    class Game
+    {
+    private:
+        bool running; // Whether the game is running
+        bool saved;   // Whether save data is available
+
+    public:
+        // The current game state
+        GAME_STATE state;
+
+        Player player = Player(FileSystem::get_path("test-rpg/data/Player.char").c_str());
+
+        // Default Game Class Constructor
+        Game();
+
+        // Returns running status
+        bool get_running();
+
+        // Runs the game loop
+        void game_loop();
+
+        // Main Menu Function
+        void run_main_menu();
+
+        // Options Menu Function
+        void run_options();
+
+        // Quits the Game
+        void quit_game();
+
+        // Runs the rpg map mode
+        void run_game();
+
+        // Starts a new battle
+        void start_battle();
+
+        // Pauses the game
+        void pause_game();
+    };
+
+} // namespace rpgText
 
 #endif // !RPG_GAME_H
--- test-rpg/src/Game.cpp
+++ test-rpg/src/Game.cpp
@@ -1,5 +1,236 @@
 #include "Game.h"
 
-Game::Game()
+namespace rpgText
 {
-}
+    Game::Game()
+    {
+        state = STATE_MENU;
+        running = true;
+        saved = false;
+    }
+
+    bool Game::get_running()
+    {
+        return running;
+    }
+
+    void Game::game_loop()
+    {
+        clear_screen();
+        switch (state)
+        {
+        case STATE_MENU:
+            run_main_menu();
+            break;
+        case STATE_OPTIONS:
+            run_options();
+            break;
+        case STATE_QUIT:
+            quit_game();
+            running = false;
+            break;
+        case STATE_MAP:
+            run_game();
+            break;
+        default:
+            running = false;
+        }
+    }
+
+    void Game::run_main_menu()
+    {
+        print_title();
+        print_menu(saved);
+        int choice = -1;
+        while (choice == -1)
+        {
+            choice = get_int();
+            switch (choice)
+            {
+            case 1:
+            case 2:
+            case 3:
+                break;
+            default:
+                choice = -1;
+                print("Wrong option! Select Again: ");
+            }
+        }
+        new_line();
+        switch (choice)
+        {
+        case 1:
+            state = STATE_MAP;
+            break;
+        case 2:
+            state = STATE_OPTIONS;
+            break;
+        case 3:
+            state = STATE_QUIT;
+            break;
+        }
+    }
+
+    void Game::run_options()
+    {
+        print_options();
+        int choice = -1;
+        while (choice == -1)
+        {
+            choice = get_int();
+            switch (choice)
+            {
+            case 1:
+            case 2:
+                break;
+            default:
+                choice = -1;
+                print("Wrong option! Select Again: ");
+            }
+        }
+        new_line();
+        if (choice == 1)
+        {
+            player.print_stats();
+            choice = get_int_prompt("Enter any number to go back to menu: ");
+        }
+        state = STATE_MENU;
+    }
+
+    void Game::quit_game()
+    {
+        print_end();
+        int choice = get_int();
+        clear_screen();
+    }
+
+    void Game::run_game()
+    {
+        bool gameRunning = true;
+        log("You have landed on a new world! This is you character:-");
+        log("");
+        player.print_stats();
+        log("");
+        while (gameRunning)
+        {
+            int choice = -1;
+            log("What do you want to do?");
+            log("1-> Travel to a new location");
+            log("2-> Pause the game to access options");
+            log("3-> Quit to Main Menu");
+            print("Choose by entering linked number:- ");
+            while (choice == -1)
+            {
+                choice = get_int();
+                switch (choice)
+                {
+                case 1:
+                case 2:
+                case 3:
+                    break;
+                default:
+                    choice = -1;
+                    print("Wrong option! Select Again: ");
+                    break;
+                }
+            }
+            clear_screen();
+            if (choice == 1)
+            {
+                choice = -1;
+                log("Choose one of the 4 directions to move in:- ");
+                log("1-> North");
+                log("2-> East");
+                log("3-> South");
+                log("4-> West");
+                print("Choose by entering linked number:- ");
+                while (choice == -1)
+                {
+                    choice = get_int();
+                    switch (choice)
+                    {
+                    case 1:
+                        log("You moved North");
+                        break;
+                    case 2:
+                        log("You moved East");
+                        break;
+                    case 3:
+                        log("You moved South");
+                        break;
+                    case 4:
+                        log("You moved West");
+                        break;
+                    default:
+                        choice = -1;
+                        print("Wrong option! Select Again: ");
+                    }
+                }
+                print_line();
+                new_line();
+                for (int i = 0; i < 10; i++)
+                {
+                    if (rpgText::check_event(20))
+                    {
+                        state = STATE_BATTLE;
+                        start_battle();
+                        if (state == STATE_MENU)
+                        {
+                            player.reset_stats();
+                            gameRunning = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (choice == 2)
+            {
+                state = STATE_PAUSE;
+                pause_game();
+            }
+            else if (choice == 3)
+            {
+                state = STATE_MENU;
+                gameRunning = false;
+            }
+            clear_screen();
+        }
+    }
+
+    void Game::start_battle()
+    {
+        clear_screen();
+        log("You encountered an enemy");
+        print_line();
+        initiate_battle(player);
+        print_line();
+        state = (player.get_status()) ? STATE_MAP : STATE_MENU;
+    }
+
+    void Game::pause_game()
+    {
+        print_pause();
+        int choice = -1;
+        while (choice == -1)
+        {
+            choice = get_int();
+            switch (choice)
+            {
+            case 1:
+            case 2:
+                break;
+            default:
+                choice = -1;
+                print("Wrong option! Select Again: ");
+            }
+        }
+        new_line();
+        if (choice == 1)
+        {
+            player.print_stats();
+            choice = get_int_prompt("Enter any number to go back to map: ");
+        }
+        state = STATE_MAP;
+    }
+
+} // namespace rpgText
